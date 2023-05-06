81e9a4a55c3055a6e4b4dd8052bcb88d3cd372d2
KuroKaraage <MantoFox@users.noreply.github.com>
2023-05-06 11:01:05 +0900

    [test] (green) Black: 5 5 5 5  White: 2 2 2 2

---
+++ TDD_Sibala/SibalaGame.cs
@@ -0,0 +1,14 @@
+﻿namespace TDD_Sibala
+{
+    internal class SibalaGame
+    {
+        public SibalaGame()
+        {
+        }
+
+        internal string ShowResult(string input)
+        {
+            return "Black wins. - with all of a kind: 5";
+        }
+    }
+}
---
+++ TDD_Sibala/SibalaGameTest.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+
+namespace TDD_Sibala
+{
+    [TestFixture]
+    internal class SibalaGameTest
+    {
+        private SibalaGame _target;
+
+        [SetUp]
+        public void @SetUp()
+        {
+            _target = new SibalaGame();
+        }
+
+        [Test]
+        public void A01_SibalaGame_ShowResult()
+        {
+            var actual = _target.ShowResult("Black: 5 5 5 5  White: 2 2 2 2");
+            var expected = "Black wins. - with all of a kind: 5";
+
+            AssertShowResultShouldReturn(actual, expected);
+        }
+
+        private static void AssertShowResultShouldReturn(string actual, string expected)
+        {
+            actual.Should().Be(expected);
+        }
+    }
+}
