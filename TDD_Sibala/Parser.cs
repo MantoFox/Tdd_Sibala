52b0560aefba7c8be9392ea2b50b4f475f8fa6d9
KuroKaraage <MantoFox@users.noreply.github.com>
2023-05-06 11:52:32 +0900

    [refactor] Parse

--- TDD_Sibala/Parser.cs
+++ TDD_Sibala/Parser.cs
@@ -4,19 +4,21 @@
     {
         internal List<Player> Parse(string input)
         {
+            var playersSection = input.Split("  ", StringSplitOptions.RemoveEmptyEntries);
+            var player1 = new Player();
+            player1.Name = playersSection[0].Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
+            var dicesSection = playersSection[0].Split(":", StringSplitOptions.RemoveEmptyEntries)[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            player1.Dices = new Dices(new List<Dice>
+                    {
+                        new Dice {Value = int.Parse(dicesSection[0]), Output = dicesSection[0]},
+                        new Dice {Value = int.Parse(dicesSection[1]), Output = dicesSection[1]},
+                        new Dice {Value = int.Parse(dicesSection[2]), Output = dicesSection[2]},
+                        new Dice {Value = int.Parse(dicesSection[3]), Output = dicesSection[3]},
+                    });
+
             return new List<Player>
             {
-                new Player
-                {
-                    Name = "Black",
-                    Dices = new Dices(new List<Dice>
-                    {
-                        new Dice {Value = 5, Output = "5"},
-                        new Dice {Value = 5, Output = "5"},
-                        new Dice {Value = 5, Output = "5"},
-                        new Dice {Value = 5, Output = "5"}
-                    })
-                }
+                player1
             };
         }
     }
