5539049682e6b3134509849b1611d764f8262895
KuroKaraage <MantoFox@users.noreply.github.com>
2023-05-07 12:55:42 +0900

    [test] (green)
            [TestCase("Black: 3 3 3 3  White: 3 3 3 3", "Tie.")]
            [TestCase("Black: 4 4 4 4  White: 5 5 5 5", "Black wins. - with all of a kind: 4")]
            [TestCase("Black: 6 6 6 6  White: 4 4 4 4", "White wins. - with all of a kind: 4")]
            [TestCase("Black: 4 4 4 4  White: 1 1 1 1", "White wins. - with all of a kind: 1")]

--- TDD_Sibala/SibalaGame.cs
+++ TDD_Sibala/SibalaGame.cs
@@ -14,7 +14,17 @@
             string description = string.Empty;
             string winnerName = string.Empty;
 
-            int compareResult = players[0].Dices.First().Value - players[1].Dices.First().Value;
+            Dictionary<int, int> valueMap = new Dictionary<int, int>
+            {
+                { 1, 6 },
+                { 2, 1 },
+                { 3, 2 },
+                { 4, 5 },
+                { 5, 3 },
+                { 6, 4 },
+            };
+            int compareResult = valueMap[players[0].Dices.First().Value] - valueMap[players[1].Dices.First().Value];
+
             if (compareResult > 0)
             {
                 winnerName = players[0].Name;
@@ -24,6 +34,10 @@
             {
                 winnerName = players[1].Name;
                 description = $"all of a kind: {players[1].Dices.First().Output}";
+            }
+            else
+            {
+                return "Tie.";
             }
 
             // Black wins. - with all of a kind: 5
