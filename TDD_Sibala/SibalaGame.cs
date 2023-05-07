43b8cb60596c8184a5411a3593b9de6b6c373904
KuroKaraage <MantoFox@users.noreply.github.com>
2023-05-07 12:40:40 +0900

    [test] (green) Black: 3 3 3 3  White: 6 6 6 6", "White wins. - with all of a kind: 6

--- TDD_Sibala/SibalaGame.cs
+++ TDD_Sibala/SibalaGame.cs
@@ -20,6 +20,11 @@
                 winnerName = players[0].Name;
                 description = $"all of a kind: {players[0].Dices.First().Output}";
             }
+            else if (compareResult < 0)
+            {
+                winnerName = players[1].Name;
+                description = $"all of a kind: {players[1].Dices.First().Output}";
+            }
 
             // Black wins. - with all of a kind: 5
             return $"{winnerName} wins. - with {description}";
