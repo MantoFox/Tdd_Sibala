f6b456d45222859675f9f358d089751ab1c90c83
KuroKaraage <MantoFox@users.noreply.github.com>
2023-05-07 12:33:11 +0900

    [refactor] ShowResult

--- TDD_Sibala/SibalaGame.cs
+++ TDD_Sibala/SibalaGame.cs
@@ -8,7 +8,22 @@
 
         internal string ShowResult(string input)
         {
-            return "Black wins. - with all of a kind: 5";
+            // Black: 5 5 5 5  White: 2 2 2 2
+            var parser = new Parser();
+            var players = parser.Parse(input);
+            string description = string.Empty;
+            string winnerName = string.Empty;
+
+            int compareResult = players[0].Dices.First().Value - players[1].Dices.First().Value;
+            if (compareResult > 0)
+            {
+                winnerName = players[0].Name;
+                description = $"all of a kind: {players[0].Dices.First().Output}";
+            }
+
+            // Black wins. - with all of a kind: 5
+            // {winner name} wins. - with {description}
+            return $"{winnerName} wins. - with {description}";
         }
     }
 }
