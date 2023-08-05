80e7674b59ca06df5588a195a94eaeb284686c2a
KuroKaraage <MantoFox@users.noreply.github.com>
2023-08-05 20:37:13 +0900

    [test] (green)
            [TestCase("Black: 3 3 3 3  White: 3 3 3 3", "Tie.")]
            [TestCase("Black: 4 4 4 4  White: 5 5 5 5", "Black wins. - with all of a kind: 4")]
            [TestCase("Black: 6 6 6 6  White: 4 4 4 4", "White wins. - with all of a kind: 4")]
            [TestCase("Black: 4 4 4 4  White: 1 1 1 1", "White wins. - with all of a kind: 1")]

--- TDD_Sibala/SibalaGame.cs
+++ TDD_Sibala/SibalaGame.cs
@@ -6,10 +6,18 @@
         {
             var parser = new Parser();
             var players = parser.Parse(input);
-            string description = string.Empty;
-            string winnerName = string.Empty;
-
-            int compareResult = players[0].Dices.First().Value - players[1].Dices.First().Value;
+            Dictionary<int, int> valueMap = new Dictionary<int, int>
+            {
+                { 1, 6 },
+                { 2, 1 },
+                { 3, 2 },
+                { 4, 5 },
+                { 5, 3 },
+                { 6, 4 },
+            };
+            int compareResult = valueMap[players[0].Dices.First().Value] - valueMap[players[1].Dices.First().Value];
+            string description;
+            string winnerName;
             if (compareResult > 0)
             {
                 winnerName = players[0].Name;
@@ -19,6 +27,10 @@
             {
                 winnerName = players[1].Name;
                 description = $"all of a kind: {players[1].Dices.First().Output}";
+            }
+            else
+            {
+                return "Tie.";
             }
 
             return $"{winnerName} wins. - with {description}";
--- TDD_Sibala/Test/SibalaGameTest.cs
+++ TDD_Sibala/Test/SibalaGameTest.cs
@@ -16,6 +16,10 @@ namespace TDD_Sibala.Test
         [Test]
         [TestCase("Black: 5 5 5 5  White: 2 2 2 2", "Black wins. - with all of a kind: 5")]
         [TestCase("Black: 3 3 3 3  White: 6 6 6 6", "White wins. - with all of a kind: 6")]
+        [TestCase("Black: 3 3 3 3  White: 3 3 3 3", "Tie.")]
+        [TestCase("Black: 4 4 4 4  White: 5 5 5 5", "Black wins. - with all of a kind: 4")]
+        [TestCase("Black: 6 6 6 6  White: 4 4 4 4", "White wins. - with all of a kind: 4")]
+        [TestCase("Black: 4 4 4 4  White: 1 1 1 1", "White wins. - with all of a kind: 1")]
         public void A01_SibalaGame_ShowResult(string input, string expected)
         {
             var actual = _target.ShowResult(input);
