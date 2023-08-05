18f6a69337d740bcbd0c8b9dc349ab2ce46906ab
KuroKaraage <MantoFox@users.noreply.github.com>
2023-08-05 20:18:30 +0900

    [test] (green) Black: 5 5 5 5  White: 2 2 2 2

---
+++ TDD_Sibala/SibalaGame.cs
@@ -0,0 +1,10 @@
+﻿namespace TDD_Sibala
+{
+    internal class SibalaGame
+    {
+        internal string ShowResult(string input)
+        {
+            return "Black wins. - with all of a kind: 5";
+        }
+    }
+}
